e4def8f334e5c595a4e92aea9fbcfbcaa62093dd
Erox1 <InfinityWaffles@outlook.com>
2021-09-06 23:11:50 -0700

    Implemented The Heartbeat Timing
    Implemented the heartbeat timing that executes when the user changes their selection or when they exit the plugin or the program.

--- code.ts
+++ code.ts
@@ -4,26 +4,27 @@ let apiKey     : string       = null,
 
 const VERSION = '1.0.0';
 
-async function sendHeartbeat (file : string, time : number, project : string, isWrite : boolean, lines : number) {
+async function sendHeartbeat (file : string, project : string, isWrite : boolean, lines : number) {
   if (apiKey != null) {
     let language = figma.editorType;
+    let time = Date.now();
 
-    figma.showUI(__html__, {visible : false});
-    figma.ui.postMessage(
-      { 
-        type: 'heartbeat', 
-        data: {
-          file,
-          time,
-          project,
-          language,
-          isWrite,
-          lines,
-          VERSION,
-          apiKey,
-        }
-      }
-    )
+    // FIXME: Causes a 405 error (not sure why)
+    // figma.ui.postMessage(
+    //   { 
+    //     type: 'heartbeat', 
+    //     data: {
+    //       file,
+    //       time,
+    //       project,
+    //       language,
+    //       isWrite,
+    //       lines,
+    //       VERSION,
+    //       apiKey,
+    //     }
+    //   }
+    // )
 
     lastAction = time;
     lastFile = file;
@@ -33,7 +34,7 @@ async function sendHeartbeat (file : string, time : number, project : string, is
       if (apiKey == null) {
         promptForAPIKey();
       } else {
-        sendHeartbeat(file, time, project, isWrite, lines);
+        sendHeartbeat(file, project, isWrite, lines);
       }
     })
   }
@@ -63,16 +64,44 @@ async function checkAPIKey () {
   })
 }
 
+function setupEventListeners() {
+  // -=- Handle Edits -=-
+  figma.on(
+    "selectionchange", 
+    () => {
+      let file = `${figma.root.name}/${figma.currentPage.name}`
+      if (enoughTimePassed() || lastFile !== file) {
+        sendHeartbeat(file, figma.root.name, false, 1)
+      }
+    }
+  );
+
+  // -=- Handle Page Saving's -=-
+  figma.on(
+    "currentpagechange", 
+    () => sendHeartbeat(`${figma.root.name}/${figma.currentPage.name}`, figma.root.name, true, 1)
+  )
+  figma.on(
+    "close", 
+    () => sendHeartbeat(`${figma.root.name}/${figma.currentPage.name}`, figma.root.name, true, 1)
+  );
+}
+
 function initialize() {
+  // -=- Load The HTML -=-
+  figma.showUI(__html__, {visible : false});
+
   // -=- Check For API Key Create A Prompt If It's Empty -=-
   checkAPIKey();
 
-  // -=- Send Test Heartbeat -=-
-  sendHeartbeat("./test", 1000, "Figma-Test-Project", true, 100); // FIXME: Causes a 405 error (not sure why)
-
   // -=- Setup Event Listeners For File Changes -=-
-  // TODO: Setup Event Listeners To Wait For The figma.viewport.center or figma.viewport.bounds
+  setupEventListeners();
+
+  // -=- Implement The Users Preferences -=-
+  // TODO: Load The Preferences
+  // TODO: Create A UI For Hiding Files
+  // TODO: Create A UI For Preferences
 }
 
 
-initialize()
+initialize();
