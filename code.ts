f97c52a768b5ed3c8fcfab9c1ae761fdfed66cc7
Erox1 <InfinityWaffles@outlook.com>
2021-09-06 19:50:12 -0700

    Added The Heartbeat Function To Call The API
    Added the heartbeat function to call the api. (giving a 405 error right now even though the paramater's are the same as the ones shown here https://github.com/wakatime/brackets-wakatime/blob/master/main.js)

--- code.ts
+++ code.ts
@@ -1,23 +1,73 @@
+let apiKey     : string       = null,
+    lastAction : number       = 0,
+    lastFile   : string       = undefined; 
+
+const VERSION = '1.0.0';
+
+async function sendHeartbeat (file : string, time : number, project : string, isWrite : boolean, lines : number) {
+  if (apiKey != null) {
+    let language = figma.editorType;
+
+    figma.showUI(__html__, {visible : false});
+    figma.ui.postMessage(
+      { 
+        type: 'heartbeat', 
+        data: {
+          file,
+          time,
+          project,
+          language,
+          isWrite,
+          lines,
+          VERSION,
+          apiKey,
+        }
+      }
+    )
+
+    lastAction = time;
+    lastFile = file;
+  } else {
+    await figma.clientStorage.getAsync("waka_time_api_key").then(myAPIKey => {
+      apiKey = myAPIKey;
+      if (apiKey == null) {
+        promptForAPIKey();
+      }
+    })
+  }
+}
+
+function enoughTimePassed() {
+  return lastAction + 120000 < Date.now();
+}
+
 function promptForAPIKey () : void {
   figma.showUI(__html__);
   figma.ui.onmessage = msg => {
     if (msg.type === 'update-api-key') {
-      const apiKey = msg.apiKey;
-      figma.clientStorage.setAsync("waka_time_api_key", apiKey);
-      figma.ui.close();
+      figma.clientStorage.setAsync("waka_time_api_key", msg.apiKey);
+      apiKey = msg.apiKey;
+      figma.ui.hide();
     }
   }
 }
 
 async function checkAPIKey () {
-  await figma.clientStorage.getAsync("waka_time_api_key").then(apiKey => {
+  await figma.clientStorage.getAsync("waka_time_api_key").then(myAPIKey => {
+    apiKey = myAPIKey;
     if (apiKey == null) {
       promptForAPIKey();
     }
-  });
+  })
 }
 
-if (figma.editorType === 'figma') {
-  checkAPIKey(); // Check if the api key is already set if not ask the user for it
-} else {
+function initialize() {
+  // -=- Check For API Key Create A Prompt If It's Empty -=-
+  checkAPIKey(); // FIXME: Causes a 405 error (not sure why)
+
+  // -=- Setup Event Listeners For File Changes -=-
+  // TODO: Setup Event Listeners To Wait For The figma.viewport.center or figma.viewport.bounds
 }
+
+
+initialize()
